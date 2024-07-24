70da24569c79698bb0c267801b160db19647f6eb
Gabriel Chaves <Remedial2344@proton.me>
2024-07-24 15:05:25 -0300

    habilitado cors para a origin url do front-end

--- terraform/infrastructure/apis/dashboard_api/ssm.tf
+++ terraform/infrastructure/apis/dashboard_api/ssm.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "frontend_url" {
   name        = "front_url"
   description = "Front-end URL"
   type        = "String"
-  value       = "http://localhost:3000"
+  value       = "http://localhost:5173"
 
   tags = {
     environment = "dev"
