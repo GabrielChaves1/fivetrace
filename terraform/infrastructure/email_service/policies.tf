d41e3dc7bcbd67183931251df0900ade3564dee7
Gabriel Chaves <Remedial2344@proton.me>
2024-07-24 01:49:46 -0300

    adicionado infraestrutura da lambda de enviar email

---
+++ terraform/infrastructure/email_service/policies.tf
@@ -0,0 +1,80 @@
+data "aws_iam_policy_document" "send_email_policy" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "ses:SendEmail",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      variable = "ses:FromAddress"
+      values   = [aws_sesv2_email_identity.sender_email_identity.email_identity]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "send_email_policy" {
+  name_prefix = "LuminogSendEmail"
+  policy      = data.aws_iam_policy_document.send_email_policy.json
+}
+
+data "aws_iam_policy_document" "send_logs_policy" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "send_logs_policy" {
+  name_prefix = "LuminogSendLogs"
+  policy      = data.aws_iam_policy_document.send_logs_policy.json
+}
+
+data "aws_iam_policy_document" "email_sender_queue_receive_message_policy" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
+    ]
+
+    resources = [aws_sqs_queue.email_sender_queue.arn]
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "email_sender_queue_receive_message_policy" {
+  name_prefix = "LuminogEmailSenderQueueReceiveMessage"
+  policy      = data.aws_iam_policy_document.email_sender_queue_receive_message_policy.json
+}
+
+data "aws_iam_policy_document" "email_sender_parameter_read_policy" {
+  statement {
+    actions = [
+      "ssm:GetParameter"
+    ]
+
+    resources = [
+      aws_ssm_parameter.sender_email_identity.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "email_sender_parameter_read_policy" {
+  name_prefix = "LuminogEmailSenderParameterRead"
+  policy      = data.aws_iam_policy_document.email_sender_parameter_read_policy.json
+}
