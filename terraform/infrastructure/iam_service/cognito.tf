43ddd7a54a5e035b196c6370e2a02ed7cc95d9d6
Gabriel Chaves <Remedial2344@proton.me>
2024-07-24 00:50:07 -0300

    adicionado infraestrutura do cognito identity manager

---
+++ terraform/infrastructure/iam_service/cognito.tf
@@ -0,0 +1,63 @@
+resource "aws_cognito_user_pool" "cognito_user_pool" {
+  name = "${local.project_name}-user-pool"
+
+  username_attributes = ["email"]
+
+  schema {
+    name                = "email"
+    attribute_data_type = "String"
+    required            = true
+    mutable             = true
+  }
+
+  schema {
+    name                = "role"
+    attribute_data_type = "String"
+    mutable             = true
+    required            = false
+  }
+
+  lifecycle {
+    ignore_changes = [
+      schema
+    ]
+  }
+
+  lambda_config {
+    post_confirmation = aws_lambda_function.signup_confirmation_lambda.arn
+  }
+}
+
+resource "aws_cognito_user_pool_domain" "pool_domain" {
+  domain       = local.project_name
+  user_pool_id = aws_cognito_user_pool.cognito_user_pool.id
+}
+
+resource "aws_cognito_resource_server" "cognito_resource_server" {
+  user_pool_id = aws_cognito_user_pool.cognito_user_pool.id
+  name         = "${local.project_name}-server"
+  identifier   = "${local.project_name}-resource-server"
+
+  scope {
+    scope_name        = "read"
+    scope_description = "for GET requests"
+  }
+
+  scope {
+    scope_name        = "write"
+    scope_description = "for POST requests"
+  }
+}
+
+resource "aws_cognito_user_pool_client" "cognito_user_pool_client_dashboard" {
+  name         = "${local.project_name}-dashboard"
+  user_pool_id = aws_cognito_user_pool.cognito_user_pool.id
+
+  generate_secret = true
+
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_USER_SRP_AUTH"
+  ]
+}
